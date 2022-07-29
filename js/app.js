74c53b3e0e4a18b2f13650d7981f330901b88606
jason606 <stonegee606@yahoo.com>
2022-07-29 22:24:30 +0000

    elements positioned on the canvas and added text styling and animations

--- js/app.js
+++ js/app.js
@@ -1,23 +1,35 @@
+const page = document.querySelector(".site");
+const parallax = document.querySelectorAll(".parallax");
 
-const parallax = document.querySelectorAll(".parallax")
 
-const io = new IntersectionObserver(entries => {
-   entries.forEach((entry) => {
-    if(entry.intersectionRatio > 0) {
-        const element = entry.target
-        // element.style.transform = 'translate3d(0, +' + (10) + 'px ,0)'
+// Parallax
+
+const io = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      const element = entry.target;
+      // element.style.transform = 'translate3d(0, +' + (10) + 'px ,0)'
     }
-   })
-    
-})
+  });
+});
 
-window.addEventListener('scroll', ()=> {
-    parallax.forEach(element => {
-        io.observe(element)
-    })
-})
+window.addEventListener("scroll", () => {
+  parallax.forEach((element) => {
+    io.observe(element);
+  });
+});
+
+// Loading Animation
+
+const loadingAnimation = () => {
+  page.classList.add("ready");
+};
+
+window.addEventListener("load", loadingAnimation());
 
 
+
+// Forms
 
 const form = document.getElementById("my-form");
 
@@ -25,23 +37,24 @@ const handleSubmit = async (e) => {
   const status = document.getElementById("my-form-status");
   e.preventDefault();
   try {
-      const res = await fetch(e.target.action, {
-        method: form.method,
-        body: new FormData(e.target),
-        headers: {
-          Accept: "application/json",
-        },
-      });
-      if (res.ok) {
-        status.innerHTML = "Thanks for your submission";
-        form.reset();
-      }else {
-        const data = await res.json();
-        console.log(data)
-        status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-      }
-      
-  }catch (e) {
+    const res = await fetch(e.target.action, {
+      method: form.method,
+      body: new FormData(e.target),
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    if (res.ok) {
+      status.innerHTML = "Thanks for your submission";
+      form.reset();
+    } else {
+      const data = await res.json();
+      console.log(data);
+      status.innerHTML = data["errors"]
+        .map((error) => error["message"])
+        .join(", ");
+    }
+  } catch (e) {
     status.innerHTML = "Oops! There was a problem submitting your form";
   }
 };
