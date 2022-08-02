7dc278e4073b31dcd37a5da357122cc245624587
jason606 <stonegee606@yahoo.com>
2022-08-02 15:46:44 +0000

    Update

--- js/app.js
+++ js/app.js
@@ -1,33 +1,66 @@
-const page = document.querySelector(".site");
-const parallax = document.querySelectorAll(".parallax");
+// Smoth Scroll
 
+const smoothScroll = () => {
 
-// Parallax
+  const page = document.querySelector(".site")
 
-const io = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.intersectionRatio > 0) {
-      const element = entry.target;
-      // element.style.transform = 'translate3d(0, +' + (10) + 'px ,0)'
-    }
-  });
-});
+  const images = Array.from(document.querySelectorAll(".img-wrap"));
+  const sconfigs = {
+    ease: 0.1,
+    current: 0,
+    target: 0,
+    previous: 0,
+    round: 0,
+  };
 
-window.addEventListener("scroll", () => {
-  parallax.forEach((element) => {
-    io.observe(element);
-  });
-});
+  // const lerp = (start, end, t) => {
+  //   return start * (1 - t) + end * t;
+  // };
+
+  const setTransforms = (el, transforms) => {
+    el.style.transform = transforms;
+    console.log(el.style.transform);
+  };
+
+  const setAnimation = () => {
+    const windowWidth = window.innerWidth;
+    const containerHeight = page.getBoundingClientRect().height;
+      sconfigs.windowWidth > 760 ? images.length / 2 : images.length;
+
+    document.body.style.height = `${sconfigs.containerHeight}px`;
+  };
+
+  const startScroll = () => {
+    sconfigs.current = window.scrollY;
+    sconfigs.previous = (sconfigs.current - sconfigs.previous) * sconfigs.ease;
+    sconfigs.round = Math.round(sconfigs.previous * 100) / 100;
+    // sconfigs.current = parseFloat(current.toFixed(2))
+
+    const difference = sconfigs.current - sconfigs.round;
+    const acceleration = size.width;
+    console.log(current);
+    setTransforms(page, `translateY(${-current}px)`);
+    requestAnimationFrame(startScroll);
+  };
+
+  setAnimation();
+};
 
 // Loading Animation
 
-const loadingAnimation = () => {
-  page.classList.add("ready");
-};
+// Parallax Animation
+// const io = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.intersectionRatio > 0) {
+//       const element = entry.target;
+//       // element.style.transform = 'translate3d(0, +' + (10) + 'px ,0)'
+//     }
+//   });
+// });
 
-window.addEventListener("load", loadingAnimation());
-
-
+// scrollObs.parallax.forEach((element) => {
+//   io.observe(element);
+// });
 
 // Forms
 
@@ -60,3 +93,10 @@ const handleSubmit = async (e) => {
 };
 
 form.addEventListener("submit", handleSubmit);
+
+const loadingAnimation = () => {
+  document.body.classList.add("ready");
+};
+
+window.addEventListener("load", loadingAnimation());
+smoothScroll();
